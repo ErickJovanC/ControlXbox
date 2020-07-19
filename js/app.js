f5ae863bd65c87c59af1aec916a2536a9f992f8a
Erick Jovan <erickjovan106@gmail.com>
2020-07-19 01:04:05 -0500

    Finalizamos el botón Contar

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 // Variables y constantes
 const inicio = document.querySelector('#inicio');
 const tiempo = document.querySelector('#tiempo');
-const boton = document.querySelector('#boton');
+const botonContar = document.querySelector('#boton');
+const botonCobrar = document.querySelector('#botonCobrar');
+const modalTiempoTotal = document.querySelector('#tiempoTotal');
+const modalPrecio = document.querySelector('#precio');
 
 let consolaActiva = false;  // Se activa al oprimir en Contar
+let contadorIntervalo;
 let contador = 1;   // Cuenta los segundos trascurridos
 let tiempo1;    // Tiempo obtenido al dar clic en el botón
 let tiempoTotal;
@@ -11,22 +15,25 @@ let precioHora = 12; // Se obtendra de un input establecido por el admin
 let precio;
 
 // Eventos
-boton.addEventListener('click', iniciaContador);
+botonContar.addEventListener('click', iniciaContador);
+botonCobrar.addEventListener('click', reiniciaContador); // Debera registar los datos en la DB
 
 // Funciones
 function iniciaContador(){
     
     if(consolaActiva==false){
         tiempo1 = iniciaTiempo();
-        console.log(tiempo1);
+        // console.log(tiempo1);
     }
     else{
-        boton.setAttribute('data-target', '#modalCobro');
+        botonContar.setAttribute('data-target', '#modalCobro');
         let tiempoFin = new Date();
         tiempoTotal = (tiempoFin-tiempo1)/1000; // Dividido / 1000 por que son milisengundos
-        console.log('Tiempo: ' + tiempoTotal);  // Segundos
+        // console.log('Tiempo: ' + tiempoTotal);  // Segundos
         precio = (tiempoTotal / 60) * (precioHora/60);  // Precio se divide en 60 para obtener precio por min.
-        console.log('Precio: ' + precio);
+        modalTiempoTotal.textContent = 'Tiempo: ' + tiempoTotal;
+        modalPrecio.textContent = 'Precio' + precio;
+        // console.log('Precio: ' + precio);
     }
 }
 
@@ -45,7 +52,7 @@ function iniciaTiempo(){
     inicio.textContent = HoraInicio + ':' + minutos + amPm;
     
     let hora =0, minuto = 0, segundo = 1;
-    /* tiempoInicio =  */setInterval(function(){
+    contadorIntervalo =  setInterval(function(){
         if(segundo==60){
             // minuto = Math.round(segundo/60);
             minuto++;
@@ -63,7 +70,16 @@ function iniciaTiempo(){
         segundo++;
     }, 1000);
 
-    boton.textContent = 'Detener';
+    botonContar.textContent = 'Detener';
     consolaActiva = true;
     return tiempoInicio.getTime();
+}
+
+function reiniciaContador(){
+    clearInterval(contadorIntervalo);
+    consolaActiva = false;
+    inicio.textContent = '0:00';
+    tiempo.textContent = '0:00';
+    botonContar.textContent = 'Contar';
+    botonContar.removeAttribute('data-target');
 }
